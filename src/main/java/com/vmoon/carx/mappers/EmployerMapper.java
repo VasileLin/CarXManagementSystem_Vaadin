d3caea478cff986fcd7e9541dcd7ef8cea953d03
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-12 13:03:51 +0200

    employer mapper

---
+++ src/main/java/com/vmoon/carx/mappers/EmployerMapper.java
@@ -0,0 +1,44 @@
+package com.vmoon.carx.mappers;
+
+import com.vmoon.carx.dto.EmployerDto;
+import com.vmoon.carx.entities.Employer;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployerMapper {
+
+    public static EmployerDto mapToEmployerDto(Employer employer) {
+        EmployerDto employerDto = EmployerDto.builder()
+                .id(employer.getId())
+                .fullName(employer.getFullName())
+                .address(employer.getAddress())
+                .phone(employer.getPhone())
+                .email(employer.getEmail())
+                .dateOfBirth(employer.getDateOfBirth())
+                .build();
+
+        if (employer.getRole() != null) {
+            employerDto.setRole(RoleMapper.mapToRoleDto(employer.getRole()));
+        }
+
+        return employerDto;
+    }
+
+
+    public static Employer mapToEmployer(EmployerDto employerDto) {
+        Employer employer = Employer.builder()
+                .id(employerDto.getId())
+                .fullName(employerDto.getFullName())
+                .address(employerDto.getAddress())
+                .phone(employerDto.getPhone())
+                .email(employerDto.getEmail())
+                .dateOfBirth(employerDto.getDateOfBirth())
+                .build();
+
+        if (employerDto.getRole() != null) {
+            employer.setRole(RoleMapper.mapToRole(employerDto.getRole()));
+        }
+
+        return employer;
+    }
+}

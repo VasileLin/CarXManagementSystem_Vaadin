9962d8195e1d86332e950b1b525d22389ca656f2
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-12 10:36:57 +0200

    main class

---
+++ src/main/java/com/vmoon/carx/Application.java
@@ -0,0 +1,29 @@
+package com.vmoon.carx;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vmoon.carx.repositories.EmployerRepository;
+import javax.sql.DataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "vmoon-carx")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}

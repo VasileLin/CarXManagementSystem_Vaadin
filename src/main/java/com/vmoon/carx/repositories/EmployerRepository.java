d4d82c52a3fd614fd9b3f488bd859b5f5f00eedc
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-12 13:04:09 +0200

    employer repository

---
+++ src/main/java/com/vmoon/carx/repositories/EmployerRepository.java
@@ -0,0 +1,19 @@
+package com.vmoon.carx.repositories;
+
+
+import com.vmoon.carx.entities.Employer;
+import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EmployerRepository extends JpaRepository<Employer, Long>, JpaSpecificationExecutor<Employer> {
+    @Query("SELECT e FROM Employer e LEFT JOIN FETCH e.role")
+    @NonNull Page<Employer> list(@NonNull Pageable pageable);
+
+}

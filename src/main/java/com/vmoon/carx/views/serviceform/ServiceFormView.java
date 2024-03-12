dc55729833d2bc9c14df613431e7eb37bf04db45
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-12 13:10:50 +0200

    service form view

---
+++ src/main/java/com/vmoon/carx/views/serviceform/ServiceFormView.java
@@ -0,0 +1,66 @@
+package com.vmoon.carx.views.serviceform;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Hr;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import com.vmoon.carx.views.MainLayout;
+
+@PageTitle("Service Form")
+@Route(value = "service-form", layout = MainLayout.class)
+@Uses(Icon.class)
+public class ServiceFormView extends Composite<VerticalLayout> {
+
+    public ServiceFormView() {
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        H3 h3 = new H3();
+        TextField textField = new TextField();
+        NumberField numberField = new NumberField();
+        Hr hr = new Hr();
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        Button buttonPrimary = new Button();
+        Button buttonSecondary = new Button();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        getContent().setJustifyContentMode(JustifyContentMode.START);
+        getContent().setAlignItems(Alignment.CENTER);
+        layoutColumn2.addClassName(Gap.SMALL);
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.setMaxWidth("800px");
+        layoutColumn2.setHeight("min-content");
+        h3.setText("Service Registration");
+        h3.setWidth("100%");
+        textField.setLabel("First Name");
+        numberField.setLabel("Price");
+        numberField.setWidth("100%");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        buttonPrimary.setText("Save");
+        buttonPrimary.setWidth("min-content");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonSecondary.setText("Cancel");
+        buttonSecondary.setWidth("min-content");
+        getContent().add(layoutColumn2);
+        layoutColumn2.add(h3);
+        layoutColumn2.add(textField);
+        layoutColumn2.add(numberField);
+        layoutColumn2.add(hr);
+        layoutColumn2.add(layoutRow);
+        layoutRow.add(buttonPrimary);
+        layoutRow.add(buttonSecondary);
+    }
+}

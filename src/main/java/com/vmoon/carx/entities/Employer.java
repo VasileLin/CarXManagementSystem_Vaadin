5a97fe886af8df1ea91523784eba9e36427f8da4
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-12 13:02:30 +0200

    Employer entity

---
+++ src/main/java/com/vmoon/carx/entities/Employer.java
@@ -0,0 +1,31 @@
+package com.vmoon.carx.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employers")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Employer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "full_name")
+    private String fullName;
+    private String phone;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+    private String address;
+    private String email;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+}

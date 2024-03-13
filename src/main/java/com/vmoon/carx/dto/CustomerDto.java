8ebf700867cbaed06d4ba54a859935ae0a37a27f
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-13 17:33:01 +0200

    customer DTO

---
+++ src/main/java/com/vmoon/carx/dto/CustomerDto.java
@@ -0,0 +1,28 @@
+package com.vmoon.carx.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomerDto {
+    private long id;
+    @NotEmpty(message = "Enter costumer name!")
+    @NotBlank(message = "Enter costumer name!")
+    private String name;
+    private String phone;
+    @NotEmpty(message = "Enter costumer car number!")
+    private String carNumber;
+    @NotEmpty(message = "Enter costumer car model!")
+    private String carModel;
+    @Email(message = "Enter an valid email")
+    @NotEmpty(message = "Enter an valid email")
+    private String email;
+}

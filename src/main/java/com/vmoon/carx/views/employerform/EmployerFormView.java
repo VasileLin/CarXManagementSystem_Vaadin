2a91fb60f0ccc97a186bf1339177a7e3e13e5e59
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-13 17:36:18 +0200

    styling fields

--- src/main/java/com/vmoon/carx/views/employerform/EmployerFormView.java
+++ src/main/java/com/vmoon/carx/views/employerform/EmployerFormView.java
@@ -10,6 +10,7 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Hr;
 import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
@@ -112,22 +113,29 @@ public class EmployerFormView extends Composite<VerticalLayout> {
         fullName = new TextField();
         fullName.setLabel("Full Name");
         fullName.setWidth("100%");
+        fullName.setPrefixComponent(new Icon(VaadinIcon.CLIPBOARD_USER));
 
         dateOfBirthPicker = new DatePicker();
         dateOfBirthPicker.setLabel("Date of Birth");
+        dateOfBirthPicker.setPrefixComponent(new Icon(VaadinIcon.CALENDAR));
 
         phoneNumber = new TextField();
         phoneNumber.setLabel("Phone Number");
+        phoneNumber.setPrefixComponent(new Icon(VaadinIcon.PHONE));
 
         roleSelect = new Select<>();
         roleSelect.setLabel("Role");
         roleSelect.setWidth("min-content");
         setSelectSampleData(roleSelect);
+        roleSelect.setPrefixComponent(new Icon(VaadinIcon.USER_CHECK));
+
 
         emailField = new EmailField();
         emailField.setLabel("Email");
+        emailField.setPrefixComponent(new Icon(VaadinIcon.MAILBOX));
 
         address = new TextArea();
+        address.setPrefixComponent(new Icon(VaadinIcon.USER_CARD));
 
 
         FormLayout formLayout2Col = new FormLayout();
@@ -136,7 +144,11 @@ public class EmployerFormView extends Composite<VerticalLayout> {
 
         saveButton = new Button();
         saveButton.addClickListener(e -> saveEmployer());
+        saveButton.setPrefixComponent(new Icon(VaadinIcon.CHECK));
+
         cancelButton = new Button();
+        cancelButton.setPrefixComponent(new Icon(VaadinIcon.CLOSE_SMALL));
+
         getContent().setWidth("100%");
         getContent().getStyle().set("flex-grow", "1");
         getContent().setJustifyContentMode(JustifyContentMode.START);
@@ -161,6 +173,8 @@ public class EmployerFormView extends Composite<VerticalLayout> {
         saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         cancelButton.setText("Cancel");
         cancelButton.setWidth("min-content");
+        cancelButton.addClickListener(e -> UI.getCurrent().navigate("employers-view"));
+
         getContent().add(layoutColumn2);
         layoutColumn2.add(h3);
         layoutColumn2.add(fullName);

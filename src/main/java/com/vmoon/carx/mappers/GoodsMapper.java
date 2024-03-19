a7a841a2e423068649f5cd5a9d254afc06310b41
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-19 17:53:50 +0200

    goods mapper

---
+++ src/main/java/com/vmoon/carx/mappers/GoodsMapper.java
@@ -0,0 +1,29 @@
+package com.vmoon.carx.mappers;
+
+import com.vmoon.carx.dto.GoodsDto;
+import com.vmoon.carx.entities.Goods;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GoodsMapper {
+
+    public static GoodsDto toGoodsDto(Goods goods) {
+        return GoodsDto.builder()
+                .id(goods.getId())
+                .costName(goods.getCostName())
+                .cost(goods.getCost())
+                .date(goods.getDate())
+                .stock(goods.getStock())
+                .build();
+    }
+
+    public static Goods toGoods(GoodsDto goodsDto) {
+        return Goods.builder()
+                .id(goodsDto.getId())
+                .costName(goodsDto.getCostName())
+                .cost(goodsDto.getCost())
+                .date(goodsDto.getDate())
+                .stock(goodsDto.getStock())
+                .build();
+    }
+}

4c335d63920132d44737a73dbd56eedeb672b6bd
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-19 17:57:43 +0200

    services service

---
+++ src/main/java/com/vmoon/carx/services/ServicesService.java
@@ -0,0 +1,18 @@
+package com.vmoon.carx.services;
+
+import com.vmoon.carx.dto.ServiceDto;
+import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ServicesService {
+
+    @NonNull Page<ServiceDto> allServices(@NonNull Pageable pageable);
+    List<ServiceDto> allServices();
+    void saveService(ServiceDto serviceDto);
+    long count();
+    Page<ServiceDto> searchService(String value, Pageable pageable);
+    long countSearchResults(String text);
+}

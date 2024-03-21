79381e24ba8ccbaac9151bd03f3d701aa266a472
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-21 14:53:43 +0200

    implemented allCashDate and countDateResult functions

--- src/main/java/com/vmoon/carx/services/implementations/CashServiceImpl.java
+++ src/main/java/com/vmoon/carx/services/implementations/CashServiceImpl.java
@@ -1,14 +1,21 @@
 package com.vmoon.carx.services.implementations;
 
 import com.vmoon.carx.dto.CashDto;
+import com.vmoon.carx.dto.CashGridDto;
 import com.vmoon.carx.mappers.CashMapper;
 import com.vmoon.carx.repositories.CashRepository;
 import com.vmoon.carx.services.CashService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CashServiceImpl implements CashService {
 
     private final CashRepository cashRepository;
@@ -18,5 +25,15 @@ public class CashServiceImpl implements CashService {
         if (customer != null) {
             cashRepository.save(CashMapper.toCash(customer));
         }
+    }
+
+    @Override
+    public Page<CashGridDto> allCashDate(PageRequest pageRequest, LocalDate fromValue, LocalDate toValue) {
+        return cashRepository.findAllByDate(pageRequest, fromValue, toValue).map(CashMapper::toCashGridDto);
+    }
+
+    @Override
+    public long countDateResult(LocalDate fromDatePickerValue, LocalDate toDatePickerValue) {
+        return cashRepository.countAllByDate(fromDatePickerValue, toDatePickerValue);
     }
 }

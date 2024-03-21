1e92725fa23f74d27d1eb82b2f922933e2c6f7d7
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-21 14:52:40 +0200

    added findAllByDate and countAllByDate functions

--- src/main/java/com/vmoon/carx/services/CashService.java
+++ src/main/java/com/vmoon/carx/services/CashService.java
@@ -1,8 +1,14 @@
 package com.vmoon.carx.services;
 
 import com.vmoon.carx.dto.CashDto;
+import com.vmoon.carx.dto.CashGridDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.time.LocalDate;
 
 public interface CashService {
-
     void saveCash(CashDto customer);
+    Page<CashGridDto> allCashDate(PageRequest pageRequest, LocalDate value, LocalDate value1);
+    long countDateResult(LocalDate value, LocalDate toDatePickerValue);
 }

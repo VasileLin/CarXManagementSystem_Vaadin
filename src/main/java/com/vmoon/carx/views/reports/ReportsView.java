ebced3a5b051cfef9ccc09789bbc9cb1e6fe84c3
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-21 15:04:05 +0200

    added cash report and costs report features

--- src/main/java/com/vmoon/carx/views/reports/ReportsView.java
+++ src/main/java/com/vmoon/carx/views/reports/ReportsView.java
@@ -12,11 +12,19 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.data.provider.DataProvider;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
 import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
-import com.vmoon.carx.entities.Employer;
+import com.vmoon.carx.dto.CashGridDto;
+import com.vmoon.carx.dto.GoodsDto;
+import com.vmoon.carx.services.CashService;
+import com.vmoon.carx.services.GoodsService;
 import com.vmoon.carx.views.MainLayout;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 
@@ -25,58 +33,232 @@ import java.time.LocalDate;
 @Uses(Icon.class)
 public class ReportsView extends Composite<VerticalLayout> {
 
-    public ReportsView() {
+    private VerticalLayout revenuesContent;
+    private VerticalLayout costsContent;
+    private final CashService cashService;
+    private final GoodsService goodsService;
+    DatePicker fromDatePicker;
+    DatePicker toDatePicker;
+    DatePicker fromGoodsDatePicker;
+    DatePicker toGoodsDatePicker;
+    Grid<GoodsDto> goodsGrid;
+    Grid<CashGridDto> cashGrid;
+
+    public ReportsView(CashService cashService, GoodsService goodsService) {
+        this.cashService = cashService;
+        this.goodsService = goodsService;
         Tabs tabs = new Tabs();
+        Tab tabRevenuesReport = new Tab("Revenues");
+        Tab tabCashReport = new Tab("Costs");
+        tabs.add(tabRevenuesReport,tabCashReport);
+
+        initializeCashReport();
+        initializeCostsReport();
+
+        tabs.addSelectedChangeListener(event -> updateVisibleContent(tabs.getSelectedTab()));
+
+        getContent().add(tabs, revenuesContent,costsContent);
+
+        updateVisibleContent(tabRevenuesReport);
+
+    }
+
+    private void initializeCostsReport() {
         HorizontalLayout layoutRow = new HorizontalLayout();
-        DatePicker datePicker = new DatePicker();
-        datePicker.setValue(LocalDate.now());
-        DatePicker datePicker2 = new DatePicker();
-        datePicker2.setValue(LocalDate.now());
+        goodsGrid = new Grid<>(GoodsDto.class,false);
+
+        fromGoodsDatePicker = new DatePicker();
+        fromGoodsDatePicker.addValueChangeListener(e -> goodsGrid.getDataProvider().refreshAll());
+        fromGoodsDatePicker.setValue(LocalDate.now());
+        toGoodsDatePicker = new DatePicker();
+        toGoodsDatePicker.setValue(LocalDate.now());
+        toGoodsDatePicker.addValueChangeListener(e -> goodsGrid.getDataProvider().refreshAll());
         VerticalLayout layoutColumn2 = new VerticalLayout();
-        Button buttonPrimary = new Button();
-        Grid basicGrid = new Grid(Employer.class);
+        Button exportButton = new Button();
         getContent().setWidth("100%");
         getContent().getStyle().set("flex-grow", "1");
-        getContent().setAlignSelf(FlexComponent.Alignment.START, tabs);
-        tabs.setWidth("100%");
-        setTabsSampleData(tabs);
         layoutRow.setWidthFull();
         getContent().setFlexGrow(1.0, layoutRow);
         layoutRow.addClassName(Gap.XLARGE);
         layoutRow.setWidth("100%");
         layoutRow.setHeight("80px");
-        datePicker.setLabel("From");
-        datePicker.setWidth("min-content");
-        datePicker2.setLabel("To");
-        datePicker2.setWidth("min-content");
+        fromGoodsDatePicker.setLabel("From");
+        fromGoodsDatePicker.setWidth("min-content");
+        toGoodsDatePicker.setLabel("To");
+        toGoodsDatePicker.setWidth("min-content");
         layoutColumn2.setHeightFull();
         layoutRow.setFlexGrow(1.0, layoutColumn2);
         layoutColumn2.setWidth("100%");
         layoutColumn2.getStyle().set("flex-grow", "1");
-        buttonPrimary.setText("Export");
-        layoutRow.setAlignSelf(FlexComponent.Alignment.END, buttonPrimary);
-        buttonPrimary.setWidth("min-content");
-        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        basicGrid.setWidth("100%");
-        basicGrid.setHeight("600px");
-        basicGrid.getStyle().set("flex-grow", "0");
-        setGridSampleData(basicGrid);
-        getContent().add(tabs);
+        exportButton.setText("Export");
+        layoutRow.setAlignSelf(FlexComponent.Alignment.END, exportButton);
+        exportButton.setWidth("min-content");
+        exportButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        goodsGrid.setWidth("100%");
+        goodsGrid.setHeight("600px");
+        goodsGrid.getStyle().set("flex-grow", "0");
+        setGoodsGridSampleData(goodsGrid);
         getContent().add(layoutRow);
-        layoutRow.add(datePicker);
-        layoutRow.add(datePicker2);
+        layoutRow.add(fromGoodsDatePicker);
+        layoutRow.add(toGoodsDatePicker);
         layoutRow.add(layoutColumn2);
-        layoutRow.add(buttonPrimary);
-        getContent().add(basicGrid);
+        layoutRow.add(exportButton);
+
+        costsContent = new VerticalLayout(layoutRow,goodsGrid);
+        costsContent.setVisible(false);
     }
 
-    private void setTabsSampleData(Tabs tabs) {
-        tabs.add(new Tab("Dashboard"));
-        tabs.add(new Tab("Payment"));
-        tabs.add(new Tab("Shipping"));
+    private void setGoodsGridSampleData(Grid<GoodsDto> goodsGrid) {
+        Grid.Column<GoodsDto> nameColumn = goodsGrid.addColumn(GoodsDto::getCostName)
+                .setHeader("Name")
+                .setResizable(true)
+                .setAutoWidth(true)
+                .setSortable(true)
+                .setSortProperty("costName");
+
+        Grid.Column<GoodsDto> costColumn = goodsGrid.addColumn(GoodsDto::getCost)
+                .setHeader("Cost")
+                .setResizable(true)
+                .setAutoWidth(true)
+                .setSortable(true)
+                .setSortProperty("cost");
+
+        Grid.Column<GoodsDto> dateColumn = goodsGrid.addColumn(GoodsDto::getDate)
+                .setHeader("Purchased date")
+                .setResizable(true)
+                .setAutoWidth(true)
+                .setSortable(true)
+                .setSortProperty("date");
+
+        Grid.Column<GoodsDto> statusColumn = goodsGrid.addColumn(GoodsDto::getStock)
+                .setHeader("Available stock")
+                .setResizable(true)
+                .setAutoWidth(true)
+                .setSortable(true)
+                .setSortProperty("stock");
+
+        goodsGrid.setColumnOrder(nameColumn,costColumn,dateColumn,statusColumn);
+
+        DataProvider<GoodsDto, Void> dataProvider = DataProvider.fromCallbacks(
+                query -> {
+                    PageRequest pageRequest = PageRequest.of(
+                            query.getPage(),
+                            query.getPageSize(),
+                            query.getSortOrders().isEmpty() ? Sort.unsorted() : VaadinSpringDataHelpers.toSpringDataSort(query)
+                    );
+
+                    Page<GoodsDto> page = goodsService.allGoodsDate(pageRequest,fromGoodsDatePicker.getValue(),toGoodsDatePicker.getValue());
+                    return page.stream();
+                },
+                query -> (int) goodsService.countDateResult(fromGoodsDatePicker.getValue(),toGoodsDatePicker.getValue())
+        );
+
+        goodsGrid.setDataProvider(dataProvider);
     }
 
-    private void setGridSampleData(Grid grid) {
+    private void updateVisibleContent(Tab selectedTab) {
+        revenuesContent.setVisible(false);
+        costsContent.setVisible(false);
+        costsContent.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        if ("Revenues".equals(selectedTab.getLabel())) {
+            revenuesContent.setVisible(true);
+        } else if ("Costs".equals(selectedTab.getLabel())) {
+            costsContent.setVisible(true);
+        }
+    }
+
+    private void initializeCashReport() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        cashGrid = new Grid<>(CashGridDto.class,false);
+
+        fromDatePicker = new DatePicker();
+        fromDatePicker.addValueChangeListener(e -> cashGrid.getDataProvider().refreshAll());
+        fromDatePicker.setValue(LocalDate.now());
+        toDatePicker = new DatePicker();
+        toDatePicker.setValue(LocalDate.now());
+        toDatePicker.addValueChangeListener(e -> cashGrid.getDataProvider().refreshAll());
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        Button exportButton = new Button();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.setWidthFull();
+        getContent().setFlexGrow(1.0, layoutRow);
+        layoutRow.addClassName(Gap.XLARGE);
+        layoutRow.setWidth("100%");
+        layoutRow.setHeight("80px");
+        fromDatePicker.setLabel("From");
+        fromDatePicker.setWidth("min-content");
+        toDatePicker.setLabel("To");
+        toDatePicker.setWidth("min-content");
+        layoutColumn2.setHeightFull();
+        layoutRow.setFlexGrow(1.0, layoutColumn2);
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        exportButton.setText("Export");
+        layoutRow.setAlignSelf(FlexComponent.Alignment.END, exportButton);
+        exportButton.setWidth("min-content");
+        exportButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        cashGrid.setWidth("100%");
+        cashGrid.setHeight("600px");
+        cashGrid.getStyle().set("flex-grow", "0");
+        setCashGridSampleData(cashGrid);
+        getContent().add(layoutRow);
+        layoutRow.add(fromDatePicker);
+        layoutRow.add(toDatePicker);
+        layoutRow.add(layoutColumn2);
+        layoutRow.add(exportButton);
+
+        revenuesContent = new VerticalLayout(layoutRow,cashGrid);
+        revenuesContent.setVisible(false);
+    }
+
+    private void setCashGridSampleData(Grid<CashGridDto> grid) {
+        Grid.Column<CashGridDto> transactionColumn = grid.addColumn(CashGridDto::getTransactionNo)
+                .setHeader("Transaction Number")
+                .setResizable(true)
+                .setAutoWidth(true)
+                .setSortable(true)
+                .setSortProperty("transactionNo");
+
+        Grid.Column<CashGridDto> dateColumn = grid.addColumn(CashGridDto::getDate)
+                .setHeader("Date")
+                .setResizable(true)
+                .setAutoWidth(true)
+                .setSortable(true)
+                .setSortProperty("date");
+
+        Grid.Column<CashGridDto> priceColumn = grid.addColumn(CashGridDto::getPrice)
+                .setHeader("Final Price")
+                .setResizable(true)
+                .setAutoWidth(true)
+                .setSortable(true)
+                .setSortProperty("price");
+
+        Grid.Column<CashGridDto> statusColumn = grid.addColumn(CashGridDto::getStatus)
+                .setHeader("Final Price")
+                .setResizable(true)
+                .setAutoWidth(true)
+                .setSortable(true)
+                .setSortProperty("status");
+
+        grid.setColumnOrder(transactionColumn,dateColumn,priceColumn,statusColumn);
+
+        DataProvider<CashGridDto, Void> dataProvider = DataProvider.fromCallbacks(
+                query -> {
+                    PageRequest pageRequest = PageRequest.of(
+                            query.getPage(),
+                            query.getPageSize(),
+                            query.getSortOrders().isEmpty() ? Sort.unsorted() : VaadinSpringDataHelpers.toSpringDataSort(query)
+                    );
+
+                    Page<CashGridDto> page = cashService.allCashDate(pageRequest,fromDatePicker.getValue(),toDatePicker.getValue());
+                    return page.stream();
+                },
+                query -> (int) cashService.countDateResult(fromDatePicker.getValue(),toDatePicker.getValue())
+        );
+
+        grid.setDataProvider(dataProvider);
     }
 
 }

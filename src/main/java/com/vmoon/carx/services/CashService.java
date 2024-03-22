0faabd51ff2b551ec4518ded897b63d46bee4b00
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-22 17:29:57 +0200

    added getCashByTransactionNo and search functions

--- src/main/java/com/vmoon/carx/services/CashService.java
+++ src/main/java/com/vmoon/carx/services/CashService.java
@@ -4,6 +4,7 @@ import com.vmoon.carx.dto.CashDto;
 import com.vmoon.carx.dto.CashGridDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
 
@@ -11,4 +12,7 @@ public interface CashService {
     void saveCash(CashDto customer);
     Page<CashGridDto> allCashDate(PageRequest pageRequest, LocalDate value, LocalDate value1);
     long countDateResult(LocalDate value, LocalDate toDatePickerValue);
+    CashDto getCashByTransactionNo(String transactionNo);
+    Page<CashGridDto> searchCash(String value, Pageable pageRequest);
+    long countSearchResults(String value);
 }

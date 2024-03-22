6c82ffe1bef0f9d1a4c718615941954404842e5c
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-22 17:30:23 +0200

    added getCashByTransactionNo and search functions implementations

--- src/main/java/com/vmoon/carx/services/implementations/CashServiceImpl.java
+++ src/main/java/com/vmoon/carx/services/implementations/CashServiceImpl.java
@@ -2,12 +2,15 @@ package com.vmoon.carx.services.implementations;
 
 import com.vmoon.carx.dto.CashDto;
 import com.vmoon.carx.dto.CashGridDto;
+import com.vmoon.carx.entities.Cash;
 import com.vmoon.carx.mappers.CashMapper;
 import com.vmoon.carx.repositories.CashRepository;
 import com.vmoon.carx.services.CashService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,5 +38,32 @@ public class CashServiceImpl implements CashService {
     @Override
     public long countDateResult(LocalDate fromDatePickerValue, LocalDate toDatePickerValue) {
         return cashRepository.countAllByDate(fromDatePickerValue, toDatePickerValue);
+    }
+
+    @Override
+    public CashDto getCashByTransactionNo(String transactionNo) {
+        return CashMapper.toCashDto(cashRepository.getCashByTransactionNo(transactionNo));
+    }
+
+    @Override
+    public Page<CashGridDto> searchCash(String value, Pageable pageable) {
+        Specification<Cash> specification = textInAllColumns(value);
+        Page<Cash> page = cashRepository.findAll(specification, pageable);
+        return page.map(CashMapper::toCashGridDto);
+    }
+
+    private Specification<Cash> textInAllColumns(String text) {
+        if (text == null || text.isEmpty()) {
+            return null;
+        }
+
+        final String finalText = "%" + text.toLowerCase() + "%";
+        return (root, query, cb) -> cb.or(
+                cb.like(cb.lower(root.get("transactionNo")), finalText));
+    }
+
+    @Override
+    public long countSearchResults(String value) {
+        return cashRepository.count(textInAllColumns(value));
     }
 }

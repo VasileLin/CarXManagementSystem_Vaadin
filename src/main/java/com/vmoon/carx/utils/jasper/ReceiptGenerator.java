c3073e3be7844836ed1955f4f60c21848a99760f
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-03-26 16:54:18 +0200

    added save receipt logic

--- src/main/java/com/vmoon/carx/utils/jasper/ReceiptGenerator.java
+++ src/main/java/com/vmoon/carx/utils/jasper/ReceiptGenerator.java
@@ -8,20 +8,35 @@ import com.vaadin.flow.server.VaadinSession;
 import net.sf.jasperreports.engine.*;
 import org.springframework.stereotype.Component;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Map;
 
 @Component
 public class ReceiptGenerator {
 
-    public static void cashReceiptGenerator(Map<String, Object> parameters) {
+    public static void cashReceiptGenerator(Map<String, Object> parameters, File pathDirectory) {
         try {
             InputStream reportStream = ReceiptGenerator.class.getResourceAsStream("/jasper/carXReceipt.jrxml");
             JasperReport jasperReport = JasperCompileManager.compileReport(reportStream);
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
 
             byte[] pdfContent = JasperExportManager.exportReportToPdf(jasperPrint);
+
+
+            if (!pathDirectory.exists()) {
+               boolean isCreated = pathDirectory.mkdirs();
+
+               if (!isCreated) {
+                   Notification.show("Failed to save receipt: ", 5000, Notification.Position.MIDDLE);
+               }
+            }
+
+            String transactionNo = parameters.get("transactionNo").toString();
+            File outputFile = new File(pathDirectory, transactionNo+ ".pdf");
+
+            try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+                fos.write(pdfContent);
+            }
 
             StreamResource resource = new StreamResource("receipt.pdf", () -> new ByteArrayInputStream(pdfContent));
             resource.setContentType("application/pdf");
@@ -32,7 +47,7 @@ public class ReceiptGenerator {
 
             UI.getCurrent().getPage().open(url, "_blank");
 
-        } catch (JRException e) {
+        } catch (JRException | IOException e) {
             Notification.show("Failed to generate the report: " + e.getMessage(), 5000, Notification.Position.MIDDLE);
         }
     }

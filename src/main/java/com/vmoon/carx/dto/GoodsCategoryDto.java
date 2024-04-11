869bc2a8df8e77b2f6687ad408bc309bcdc756cb
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-04-11 11:12:08 +0300

    added Goods Category DTO

---
+++ src/main/java/com/vmoon/carx/dto/GoodsCategoryDto.java
@@ -0,0 +1,14 @@
+package com.vmoon.carx.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GoodsCategoryDto {
+
+    private int id;
+    private String name;
+}

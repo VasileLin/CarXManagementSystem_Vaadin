0e6ad194892324a12851aae04318e87766a9f2da
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-04-11 11:21:31 +0300

    implemented find goods by category and car brand function

--- src/main/java/com/vmoon/carx/services/implementations/GoodsServiceImpl.java
+++ src/main/java/com/vmoon/carx/services/implementations/GoodsServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class GoodsServiceImpl implements GoodsService {
 
     private final GoodsRepository goodsRepository;
@@ -33,9 +35,9 @@ public class GoodsServiceImpl implements GoodsService {
     }
 
     @Override
-    public void saveGood(GoodsDto employee) {
-        if (employee != null) {
-            goodsRepository.save(GoodsMapper.toGoods(employee));
+    public void saveGood(GoodsDto goodsDto) {
+        if (goodsDto != null) {
+            goodsRepository.save(GoodsMapper.toGoods(goodsDto));
         }
     }
 
@@ -55,6 +57,11 @@ public class GoodsServiceImpl implements GoodsService {
     }
 
     @Override
+    public long countSearchResults(int categoryId, int brandId) {
+        return goodsRepository.countAllByCategoryIdAndCarBrandId(categoryId, brandId);
+    }
+
+    @Override
     public void updateStock(long id, int newStock) {
         Optional<Goods> goodsDto = goodsRepository.findById(id);
         goodsDto.ifPresent(goods -> {
@@ -65,11 +72,18 @@ public class GoodsServiceImpl implements GoodsService {
 
     @Override
     public Page<GoodsDto> allGoodsDate(PageRequest pageRequest, LocalDate fromValue, LocalDate toValue) {
-        return goodsRepository.findAllByDate(pageRequest,fromValue,toValue).map(GoodsMapper::toGoodsDto);
+        return goodsRepository.findAllByDate(pageRequest,fromValue,toValue)
+                .map(GoodsMapper::toGoodsDto);
     }
 
     @Override
     public long countDateResult(LocalDate fromValue, LocalDate toValue) {
         return goodsRepository.countAllByDate(fromValue,toValue);
+    }
+
+    @Override
+    public Page<GoodsDto> fetchGoodsForCategoryAndBrand(int categoryId, int brandId,PageRequest pageRequest) {
+        return goodsRepository.findAllByCategoryIdAndCarBrandId(categoryId,brandId,pageRequest)
+         .map(GoodsMapper::toGoodsDto);
     }
 }

71f481951077b2a212061545c36080cccb0fb227
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-04-11 11:02:08 +0300

    car brand repository

---
+++ src/main/java/com/vmoon/carx/repositories/CarBrandRepository.java
@@ -0,0 +1,7 @@
+package com.vmoon.carx.repositories;
+
+import com.vmoon.carx.entities.CarBrand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarBrandRepository extends JpaRepository<CarBrand,Integer> {
+}

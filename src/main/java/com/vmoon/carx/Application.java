c1a9167a70ec822afb11c1ab095dc650bf254a4f
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-04-17 15:00:46 +0300

    application class

--- src/main/java/com/vmoon/carx/Application.java
+++ src/main/java/com/vmoon/carx/Application.java
@@ -2,17 +2,12 @@ package com.vmoon.carx;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
-import com.vmoon.carx.repositories.EmployerRepository;
-import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
-import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
-import org.springframework.context.annotation.Bean;
 
 /**
  * The entry point of the Spring Boot application.
- *
+ * <p>
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
  *

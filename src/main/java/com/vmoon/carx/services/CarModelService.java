4fc9cfc5c9141bdc236120db6e3abe7b000622e1
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-04-17 15:09:05 +0300

    added car model service

---
+++ src/main/java/com/vmoon/carx/services/CarModelService.java
@@ -0,0 +1,11 @@
+package com.vmoon.carx.services;
+
+import com.vmoon.carx.dto.CarModelDto;
+
+import java.util.List;
+
+public interface CarModelService {
+
+    List<CarModelDto> getCarModelsByBrandId(int brandId);
+    void saveModel(CarModelDto carModelDto);
+}

97231f0ca645b047d684cf0f112e8c107b0319f1
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-04-22 16:47:42 +0300

    added pageable findAll JPA function with specifications for search functionality

--- src/main/java/com/vmoon/carx/repositories/GoodsRepository.java
+++ src/main/java/com/vmoon/carx/repositories/GoodsRepository.java
@@ -3,13 +3,16 @@ package com.vmoon.carx.repositories;
 import com.vmoon.carx.entities.Goods;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 
+@Repository
 public interface GoodsRepository extends JpaRepository<Goods, Long>, JpaSpecificationExecutor<Goods> {
     @Query("SELECT g FROM Goods g " +
             "WHERE g.date >= :fromValue AND g.date <= :toValue")
@@ -20,6 +23,8 @@ public interface GoodsRepository extends JpaRepository<Goods, Long>, JpaSpecific
     long countAllByDate(@Param("fromValue")LocalDate fromValue, @Param("toValue")LocalDate toValue);
 
     Page<Goods> findAllByCategoryIdAndCarBrandId(int categoryId, int brandId,Pageable pageable);
+
+    Page<Goods> findAll(Specification<Goods> spec, Pageable pageable);
 
     long countAllByCategoryIdAndCarBrandId(int categoryId, int brandId);
 }

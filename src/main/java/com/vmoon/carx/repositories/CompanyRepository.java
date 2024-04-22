b1cfdcb0c43a103efc4a3038bfedbd5d333d9d00
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-04-22 16:40:02 +0300

    mark interface as repository

--- src/main/java/com/vmoon/carx/repositories/CompanyRepository.java
+++ src/main/java/com/vmoon/carx/repositories/CompanyRepository.java
@@ -2,6 +2,8 @@ package com.vmoon.carx.repositories;
 
 import com.vmoon.carx.entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company,Long> {
 }

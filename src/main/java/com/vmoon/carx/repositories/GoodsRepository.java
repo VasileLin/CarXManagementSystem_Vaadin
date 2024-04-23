2951a53e982bf192e7a0d3070348f4c3a4c8fed8
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-04-23 16:27:05 +0300

    fix N+1 queries

--- src/main/java/com/vmoon/carx/repositories/GoodsRepository.java
+++ src/main/java/com/vmoon/carx/repositories/GoodsRepository.java
@@ -4,10 +4,12 @@ import com.vmoon.carx.entities.Goods;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -16,6 +18,9 @@ import java.time.LocalDate;
 public interface GoodsRepository extends JpaRepository<Goods, Long>, JpaSpecificationExecutor<Goods> {
     @Query("SELECT g FROM Goods g " +
             "WHERE g.date >= :fromValue AND g.date <= :toValue")
+
+    @NonNull
+    @EntityGraph(attributePaths = {"category","carBrand"})
     Page<Goods> findAllByDate(Pageable pageable, @Param("fromValue") LocalDate fromValue, @Param("toValue")LocalDate toValue);
 
     @Query("SELECT count(g) FROM Goods g " +
@@ -24,7 +29,9 @@ public interface GoodsRepository extends JpaRepository<Goods, Long>, JpaSpecific
 
     Page<Goods> findAllByCategoryIdAndCarBrandId(int categoryId, int brandId,Pageable pageable);
 
-    Page<Goods> findAll(Specification<Goods> spec, Pageable pageable);
+
+    @NonNull
+    Page<Goods> findAll(@NonNull Specification<Goods> spec, @NonNull Pageable pageable);
 
     long countAllByCategoryIdAndCarBrandId(int categoryId, int brandId);
 }

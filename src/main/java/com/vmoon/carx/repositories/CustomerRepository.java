ac85d659936383ac614be2bac0ba6e726a267731
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-04-23 16:25:46 +0300

    fix solving N+1 queries

--- src/main/java/com/vmoon/carx/repositories/CustomerRepository.java
+++ src/main/java/com/vmoon/carx/repositories/CustomerRepository.java
@@ -1,10 +1,24 @@
 package com.vmoon.carx.repositories;
 
 import com.vmoon.carx.entities.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long>, JpaSpecificationExecutor<Customer> {
+
+    @EntityGraph(attributePaths = {"carBrand","carModel"})
+    @NonNull
+    Page<Customer> findAll(@NonNull Pageable pageable);
+
+    @EntityGraph(attributePaths = {"carBrand","carModel"})
+    @NonNull
+    List<Customer> findAll();
 }

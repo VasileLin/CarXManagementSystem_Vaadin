c6e36e57c5a05f81aa0e5914eb11b13f197ba0f5
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-04-26 15:47:07 +0300

    goods category repo

--- src/main/java/com/vmoon/carx/repositories/GoodsCategoryRepository.java
+++ src/main/java/com/vmoon/carx/repositories/GoodsCategoryRepository.java
@@ -2,8 +2,14 @@ package com.vmoon.carx.repositories;
 
 import com.vmoon.carx.entities.GoodsCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GoodsCategoryRepository extends JpaRepository<GoodsCategory,Integer> {
+
+    @NonNull
+    List<GoodsCategory> findAll();
 }

bf14d6c7ba6517daf3cd79d0677ed785c1de8285
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-04-26 15:52:36 +0300

    show only allowed views for authenticated user

--- src/main/java/com/vmoon/carx/views/MainLayout.java
+++ src/main/java/com/vmoon/carx/views/MainLayout.java
@@ -13,23 +13,34 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.server.auth.AccessAnnotationChecker;
 import com.vaadin.flow.theme.lumo.LumoUtility;
+import com.vmoon.carx.entities.UserEntity;
+import com.vmoon.carx.security.AuthenticatedUser;
 import com.vmoon.carx.views.cash.CashView;
 import com.vmoon.carx.views.customers.CustomersView;
 import com.vmoon.carx.views.employers.EmployersView;
 import com.vmoon.carx.views.reports.ReportsView;
 import com.vmoon.carx.views.service.ServiceView;
 import com.vmoon.carx.views.settings.SettingsView;
+import com.vmoon.carx.views.users.UsersView;
 import org.vaadin.lineawesome.LineAwesomeIcon;
+
+import java.util.Optional;
 
 /**
  * The main view is a top-level placeholder for other views.
  */
 public class MainLayout extends AppLayout {
 
+    private final AuthenticatedUser authenticatedUser;
+    private final AccessAnnotationChecker accessChecker;
+
     private H2 viewTitle;
 
-    public MainLayout() {
+    public MainLayout(AuthenticatedUser authenticatedUser, AccessAnnotationChecker accessChecker) {
+        this.authenticatedUser = authenticatedUser;
+        this.accessChecker = accessChecker;
         setPrimarySection(Section.DRAWER);
         addDrawerContent();
         addHeaderContent();
@@ -68,17 +79,33 @@ public class MainLayout extends AppLayout {
     private SideNav createNavigation() {
         SideNav nav = new SideNav();
 
+        if (accessChecker.hasAccess(EmployersView.class)) {
             nav.addItem(new SideNavItem("Employers", EmployersView.class, LineAwesomeIcon.PEOPLE_CARRY_SOLID.create()));
+        }
 
+        if (accessChecker.hasAccess(CustomersView.class)) {
             nav.addItem(new SideNavItem("Customers", CustomersView.class, LineAwesomeIcon.MONEY_BILL_WAVE_SOLID.create()));
+        }
 
+        if (accessChecker.hasAccess(ServiceView.class)) {
             nav.addItem(new SideNavItem("Service", ServiceView.class, LineAwesomeIcon.CAR_BATTERY_SOLID.create()));
+        }
 
+        if (accessChecker.hasAccess(CashView.class)) {
             nav.addItem(new SideNavItem("Cash", CashView.class, LineAwesomeIcon.CASH_REGISTER_SOLID.create()));
+        }
 
+        if (accessChecker.hasAccess(ReportsView.class)) {
             nav.addItem(new SideNavItem("Reports", ReportsView.class, LineAwesomeIcon.CHART_BAR_SOLID.create()));
+        }
 
+        if (accessChecker.hasAccess(SettingsView.class)) {
             nav.addItem(new SideNavItem("Settings", SettingsView.class, LineAwesomeIcon.ICONS_SOLID.create()));
+        }
+
+        if (accessChecker.hasAccess(UsersView.class)) {
+            nav.addItem(new SideNavItem("Users", UsersView.class, LineAwesomeIcon.USERS_COG_SOLID.create()));
+        }
 
         return nav;
     }
@@ -86,27 +113,32 @@ public class MainLayout extends AppLayout {
     private Footer createFooter() {
         Footer layout = new Footer();
 
-            Avatar avatar = new Avatar("User");
-            avatar.setThemeName("xsmall");
-            avatar.getElement().setAttribute("tabindex", "-1");
+        Optional<UserEntity> maybeUser = authenticatedUser.get();
 
-            MenuBar userMenu = new MenuBar();
-            userMenu.setThemeName("tertiary-inline contrast");
+            if (maybeUser.isPresent()) {
 
-            MenuItem userName = userMenu.addItem("");
-            Div div = new Div();
-            div.add(avatar);
-            div.add("User");
-            div.add(new Icon("lumo", "dropdown"));
-            div.getElement().getStyle().set("display", "flex");
-            div.getElement().getStyle().set("align-items", "center");
-            div.getElement().getStyle().set("gap", "var(--lumo-space-s)");
-            userName.add(div);
-            userName.getSubMenu().addItem("Sign out", e -> {
-               /* authenticatedUser.logout();*/
-            });
+                UserEntity user = maybeUser.get();
+                Avatar avatar = new Avatar("User");
+                avatar.setThemeName("xsmall");
+                avatar.getElement().setAttribute("tabindex", "-1");
 
-            layout.add(userMenu);
+                MenuBar userMenu = new MenuBar();
+                userMenu.setThemeName("tertiary-inline contrast");
+
+                MenuItem userName = userMenu.addItem("");
+                Div div = new Div();
+                div.add(avatar);
+                div.add(user.getUsername());
+                div.add(new Icon("lumo", "dropdown"));
+                div.getElement().getStyle().set("display", "flex");
+                div.getElement().getStyle().set("align-items", "center");
+                div.getElement().getStyle().set("gap", "var(--lumo-space-s)");
+                userName.add(div);
+                userName.getSubMenu().addItem("Sign out", e -> authenticatedUser.logout());
+
+                layout.add(userMenu);
+            }
+
 
         return layout;
     }

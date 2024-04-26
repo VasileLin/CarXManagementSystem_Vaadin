941f500fffef4b96ea2b0feb6e0d76b046691bfd
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-04-26 15:48:29 +0300

    added entity graph attribute paths to findAll function

--- src/main/java/com/vmoon/carx/repositories/GoodsRepository.java
+++ src/main/java/com/vmoon/carx/repositories/GoodsRepository.java
@@ -13,12 +13,12 @@ import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface GoodsRepository extends JpaRepository<Goods, Long>, JpaSpecificationExecutor<Goods> {
     @Query("SELECT g FROM Goods g " +
             "WHERE g.date >= :fromValue AND g.date <= :toValue")
-
     @NonNull
     @EntityGraph(attributePaths = {"category","carBrand"})
     Page<Goods> findAllByDate(Pageable pageable, @Param("fromValue") LocalDate fromValue, @Param("toValue")LocalDate toValue);
@@ -29,6 +29,9 @@ public interface GoodsRepository extends JpaRepository<Goods, Long>, JpaSpecific
 
     Page<Goods> findAllByCategoryIdAndCarBrandId(int categoryId, int brandId,Pageable pageable);
 
+    @NonNull
+    @EntityGraph(attributePaths = {"category","carBrand"})
+    List<Goods> findAll();
 
     @NonNull
     Page<Goods> findAll(@NonNull Specification<Goods> spec, @NonNull Pageable pageable);

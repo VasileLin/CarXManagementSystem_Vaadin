44f770758e8cb754b9568b30ec06156fc01113de
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-05-02 08:44:47 +0300

    added new functions

--- src/main/java/com/vmoon/carx/services/CustomerService.java
+++ src/main/java/com/vmoon/carx/services/CustomerService.java
@@ -10,10 +10,13 @@ import java.util.List;
 public interface CustomerService {
 
     @NonNull Page<CustomerDto> allCustomers(@NonNull Pageable pageable);
-
+    @NonNull Page<CustomerDto> allDeletedCustomers(@NonNull Pageable pageable);
     List<CustomerDto> listCustomers();
     void saveCustomer(CustomerDto customer);
-    long count();
+    long countDeleted();
+    long countCustomers();
     Page<CustomerDto> searchCustomers(String value, Pageable pageable);
+    Page<CustomerDto> searchDeletedCustomers(String value, Pageable pageable);
+    long countDeletedSearchResults(String value);
     long countSearchResults(String value);
 }

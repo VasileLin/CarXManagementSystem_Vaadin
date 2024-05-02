323dd5502d8caa3af955ef58ffb8fb67839bd0fd
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-05-02 08:43:02 +0300

    removed unused import statements

--- src/main/java/com/vmoon/carx/repositories/CustomerRepository.java
+++ src/main/java/com/vmoon/carx/repositories/CustomerRepository.java
@@ -3,7 +3,6 @@ package com.vmoon.carx.repositories;
 import com.vmoon.carx.entities.Customer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;

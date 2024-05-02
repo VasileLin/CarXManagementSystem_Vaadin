8d710bee1e36bc23fff2499435c1f83f485a594a
lingavasile <144684005+lingavasile@users.noreply.github.com>
2024-05-02 08:41:55 +0300

    changed JPA queries

--- src/main/java/com/vmoon/carx/repositories/CustomerRepository.java
+++ src/main/java/com/vmoon/carx/repositories/CustomerRepository.java
@@ -3,9 +3,11 @@ package com.vmoon.carx.repositories;
 import com.vmoon.carx.entities.Customer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
@@ -15,10 +17,22 @@ import java.util.List;
 public interface CustomerRepository extends JpaRepository<Customer,Long>, JpaSpecificationExecutor<Customer> {
 
     @EntityGraph(attributePaths = {"carBrand","carModel"})
+    @Query("SELECT c FROM Customer c WHERE c.isDeleted = false")
     @NonNull
     Page<Customer> findAll(@NonNull Pageable pageable);
 
     @EntityGraph(attributePaths = {"carBrand","carModel"})
+    @Query("SELECT c FROM Customer c WHERE c.isDeleted = true")
+    @NonNull
+    Page<Customer> findAllDeleted(@NonNull Pageable pageable);
+
+    @EntityGraph(attributePaths = {"carBrand","carModel"})
     @NonNull
     List<Customer> findAll();
+
+    @Query("SELECT count(c) FROM Customer c WHERE c.isDeleted = true")
+    long countDeleted();
+
+    @Query("SELECT count(c) FROM Customer c WHERE c.isDeleted = false")
+    long count();
 }

58ed4da4931ba5639d6ada7805f08d9c3e8072f6
VasileLinga <vasile.linga1@gmail.com>
2024-07-21 13:08:02 +0300

    Add company name to email content and improve message wording

--- src/main/java/com/vmoon/carx/utils/MailTools.java
+++ src/main/java/com/vmoon/carx/utils/MailTools.java
@@ -1,5 +1,6 @@
 package com.vmoon.carx.utils;
 
+import com.vmoon.carx.services.CompanyService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -15,14 +16,17 @@ import java.io.IOException;
 @Service
 public class MailTools {
     private final JavaMailSender mailSender;
+    private final CompanyService companyService;
 
     public void sendEmailWithAttachment(String to, byte[] pdfBytes) throws MessagingException, IOException {
+        String companyName = companyService.getAllCompanies().get(0).getName();
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
         helper.setFrom("vmoon.apps@gmail.com");
         helper.setTo(to);
-        helper.setSubject("Your Receipt in electronic format");
-        helper.setText("Please find your receipt attached.");
+        helper.setSubject("Invoice for Your Recent Auto Service");
+        helper.setText("Dear Customer,\nThank you for choosing our auto service.\nPlease find attached the invoice for the services rendered.\nIf you have any questions or require further assistance, feel free to contact us.\nBest regards,"+companyName);
+
 
         File tempFile = File.createTempFile("receipt", ".pdf");
         try (FileOutputStream fos = new FileOutputStream(tempFile)) {

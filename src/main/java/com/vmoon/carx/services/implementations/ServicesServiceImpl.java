7ac37f04c8057ab4a088cf8525920146c3b6f5fe
VasileLinga <vasile.linga1@gmail.com>
2024-07-21 13:08:58 +0300

    Add features for managing deleted services

--- src/main/java/com/vmoon/carx/services/implementations/ServicesServiceImpl.java
+++ src/main/java/com/vmoon/carx/services/implementations/ServicesServiceImpl.java
@@ -1,13 +1,20 @@
 package com.vmoon.carx.services.implementations;
 
+import com.vmoon.carx.dto.EmployerDto;
 import com.vmoon.carx.dto.ServiceDto;
+import com.vmoon.carx.entities.Employer;
 import com.vmoon.carx.entities.Service;
+import com.vmoon.carx.mappers.EmployerMapper;
 import com.vmoon.carx.mappers.ServiceMapper;
 import com.vmoon.carx.repositories.ServiceRepository;
 import com.vmoon.carx.services.ServicesService;
+import jakarta.persistence.criteria.Predicate;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -16,12 +23,13 @@ import java.util.List;
 @org.springframework.stereotype.Service
 @RequiredArgsConstructor
 public class ServicesServiceImpl implements ServicesService {
-
+    private static final Logger logger = LoggerFactory.getLogger(ServicesServiceImpl.class);
     private final ServiceRepository serviceRepository;
 
     @Override
     public @NonNull Page<ServiceDto> allServices(@NonNull Pageable pageable) {
-        return serviceRepository.findAll(pageable).map(ServiceMapper.INSTANCE::toServiceDto);
+        return serviceRepository.findAll(pageable)
+                .map(ServiceMapper.INSTANCE::toServiceDto);
     }
 
     @Override
@@ -52,6 +60,45 @@ public class ServicesServiceImpl implements ServicesService {
     public long countSearchResults(String text) {
         return serviceRepository.count(textInAllColumns(text));
     }
+
+    @Override
+    public int countDeleted() {
+       return serviceRepository.countDeleted();
+    }
+
+    @Override
+    public Page<ServiceDto> allDeletedServices(Pageable pageable) {
+        logger.info("All deleted services fetched successfully");
+        return serviceRepository.listDeleted(pageable).map(ServiceMapper.INSTANCE::toServiceDto);
+    }
+
+    @Override
+    public Page<ServiceDto> searchService(String value, Pageable pageable, boolean isDeleted) {
+        Specification<Service> spec = textInAllColumns(value,isDeleted);
+        Page<Service> page = serviceRepository.findAll(spec, pageable);
+        return page.map(ServiceMapper.INSTANCE::toServiceDto);
+    }
+
+    public static Specification<Service> textInAllColumns(String text,boolean isDeleted) {
+        if (text == null || text.isEmpty()) {
+            return null;
+        }
+
+        final String finalText = "%" + text.toLowerCase() + "%";
+        return (root, query, cb) ->
+        {
+            Predicate textCondition = cb.or(
+                    cb.like(cb.lower(root.get("name")), finalText)
+            );
+
+            Predicate isDeletedCondition = cb.equal(root.get("isDeleted"), isDeleted);
+            return cb.and(textCondition, isDeletedCondition);
+
+        };
+
+
+    }
+
 
     public static Specification<Service> textInAllColumns(String text) {
         if (text == null || text.isEmpty()) {

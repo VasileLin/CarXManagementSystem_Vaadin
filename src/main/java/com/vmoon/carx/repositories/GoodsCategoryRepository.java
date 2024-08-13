0857237b853510ea6b17f56b09adfc579f7af6b1
Vasile Linga <vasile.linga@iongroup.com>
2024-08-13 16:33:33 +0300

    Implement pagination and counting for GoodsCategory in repository interface

--- src/main/java/com/vmoon/carx/repositories/GoodsCategoryRepository.java
+++ src/main/java/com/vmoon/carx/repositories/GoodsCategoryRepository.java
@@ -1,15 +1,24 @@
 package com.vmoon.carx.repositories;
 
+import com.vmoon.carx.dto.GoodsCategoryDto;
+import com.vmoon.carx.entities.Employer;
 import com.vmoon.carx.entities.GoodsCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface GoodsCategoryRepository extends JpaRepository<GoodsCategory,Integer> {
+public interface GoodsCategoryRepository extends JpaRepository<GoodsCategory,Integer>, JpaSpecificationExecutor<GoodsCategory> {
 
     @NonNull
-    List<GoodsCategory> findAll();
+    @Query("SELECT c FROM GoodsCategory c " +
+            "WHERE c.isDeleted = :isDeleted")
+    Page<GoodsCategory> allCategories(Pageable pageable, boolean isDeleted);
+
+    @Query("SELECT count(c) FROM GoodsCategory c WHERE c.isDeleted = :isDeleted")
+    long count(boolean isDeleted);
 }
